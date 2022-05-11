9f6276795280c7957151acc9d4d8e66360722608
Dmitriy Yuganuk <dmitriyyuganuk@gmail.com>
2022-05-11 13:35:34 +0300

    Btn added global

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {Input} from "./components/Input/Input";
+import {Btn} from "./components/Btn/Btn";
 
 function App() {
     return (
         <div className="App">
              Second commit
             <Input/>
+            <Btn/>
         </div>
     );
 }
